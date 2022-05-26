08cdc918272be935d8934821a79a6b1cb2189c81
majedulhaque1 <majedulhaquerakib601@gmail.com>
2022-05-26 10:51:41 +0600

    patch api create done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -14,6 +15,21 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qydu8.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const verifyJWT = (req, res, next) =>{
+    const authheader = req.headers.authorization;
+    if(!authheader){
+        return res.status(401).send({message : 'unauthorize access'});
+    }
+    const token = authheader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err){
+            return res.status(403).send({message: 'forbiden access'});
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 async function run(){
     try{
         await client.connect();
@@ -38,7 +54,7 @@ async function run(){
             const result = await productCollection.find(query).toArray();
             res.send(result);
         })
-        app.get('/products/:Id', async (req, res) =>{
+        app.get('/products/:Id', verifyJWT, async (req, res) =>{
             const id = req.params.Id;
             const query = {_id : ObjectId(id)};
             const result = await productCollection.findOne(query);
@@ -50,23 +66,42 @@ async function run(){
             const result = await productCollection.insertOne(product);
             res.send(result);
         })
-        app.get('/orders', async (req, res) =>{
-            const query = {};
-            const result = await ordersCollection.find(query).toArray();
+        app.get('/orders', verifyJWT, async (req, res) =>{
+            const buyer = req.query.buyer;
+            const filter = {email: buyer};
+            console.log(buyer);
+            const result = await ordersCollection.find(filter).toArray();
             res.send(result);
         })
         app.post('/orders', async (req, res) =>{
             const order = req.body;
+            console.log(order)
             const result = await ordersCollection.insertOne(order);
             res.send(result);
         })
 
-        app.get('/users', async (req, res) =>{
+        app.get('/users', verifyJWT, async (req, res) =>{
             const query = {};
             const result = await usersCollection.find(query).toArray();
             res.send(result);
         })
-        app.put('/users/:eamil', async (req, res) =>{
+        app.patch('/users/admin/:email', verifyJWT, async (req, res) =>{
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const filter = { email : requester};
+            console.log(requester);
+            const requesterEmail = await usersCollection.findOne(filter);
+            console.log(requesterEmail);
+            if(requesterEmail.role === 'admin'){
+                const filter = {email : email};
+                const updadedDoc = {
+                    $set : {role : 'admin'}
+                }
+                const result = await usersCollection.updateOne(filter, updadedDoc);
+                res.send(result);
+            }
+        })
+        app.put('/users/:email', async (req, res) =>{
             const email = req.params.email;
             const user = req.body;
             const filter = {email : email};
@@ -74,8 +109,9 @@ async function run(){
             const updadedDoc = {
                 $set : user
             }
+            const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn : '1h'});
             const result = await usersCollection.updateOne(filter, updadedDoc, options);
-            res.send(result);
+            res.send({result, token});
         })
     }
     finally{
