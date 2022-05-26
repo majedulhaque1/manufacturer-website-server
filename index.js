3059e63d64121b9d27e2da6264f08723838c78aa
majedulhaque1 <majedulhaquerakib601@gmail.com>
2022-05-26 22:50:33 +0600

    delete api create

--- index.js
+++ index.js
@@ -60,6 +60,12 @@ async function run(){
             const result = await productCollection.findOne(query);
             res.send(result);
         })
+        app.delete('/products/:Id', verifyJWT, async (req, res) =>{
+            const id = req.params.Id;
+            const query = {_id : ObjectId(id)};
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
 
         app.post('/products', async (req, res) =>{
             const product = req.body;
